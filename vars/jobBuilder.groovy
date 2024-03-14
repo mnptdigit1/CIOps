396778d20a3e114c6adbec2bb78e1f49638049fd
mnptdigit <149570527+mnptdigit@users.noreply.github.com>
2024-03-14 17:56:36 +0530

    Update jobBuilder.groovy
    change mnptdigit name

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -29,7 +29,7 @@ spec:
             name: jenkins-credentials
             key: dockerPassword
       - name: DOCKER_NAMESPACE
-        value: mnptdigit/mnpt-digit
+        value: mnpt-digit
       - name: DOCKER_GROUP_NAME  
         value: dev
     resources:
