1419f13fddfc3878c1f8ffb124a860932facdef9
mnptdigit <149570527+mnptdigit@users.noreply.github.com>
2024-03-19 23:06:36 +0530

    Update deployer.groovy
    update

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -53,6 +53,26 @@ spec:
                     """
                 }
             } */
+
+            stage('Export Kubeconfig Secret') {
+                container(name: 'egov-deployer', shell: '/bin/sh') {
+                       sh """
+                        # Create the .kube directory
+                        mkdir -p /root/.kube
+                        
+                        # Extract the kubeconfig from the secret and write it to a file
+                        kubectl get secret ${pipelineParams.environment}-kube-config -n jenkins -o jsonpath='{.data.config}' | base64 -d > /root/.kube/config
+                        
+                        # Optionally, set KUBECONFIG environment variable to use this kubeconfig
+                        export KUBECONFIG=/root/.kube/config
+                        kubectl config get-contexts
+                        kubectl get ns
+                        kubectl config current-context
+                        aws-iam-authenticator version
+                        kubectl get nodes
+                    """
+                }
+            }
             
             stage('Deploy Images') {
                 container(name: 'egov-deployer', shell: '/bin/sh') {
