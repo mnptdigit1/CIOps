a936496f866d60d4cb8461917e46a1ea05ec87cb
mnptdigit <149570527+mnptdigit@users.noreply.github.com>
2024-03-21 15:28:49 +0530

    Update deployer.groovy
    update deployer.groovy from upyog repo

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -1,13 +1,8 @@
 library 'ci-libs'
 
 def call(Map pipelineParams) {
-    echo "Environment: ${pipelineParams.environment}"
-    // Print out the variables
-    echo "pipelineParams.helmDir: ${pipelineParams.helmDir}"
-    echo "env.CLUSTER_CONFIGS: ${env.CLUSTER_CONFIGS}"
-    echo "pipelineParams.environment: ${pipelineParams.environment}"
-    echo "env.IMAGES: ${env.IMAGES}"
-    podTemplate(yaml: """
+
+podTemplate(yaml: """
 kind: Pod
 metadata:
   name: egov-deployer
@@ -18,69 +13,36 @@ spec:
     command:
     - cat
     tty: true
+    env:  
+      - name: "GOOGLE_APPLICATION_CREDENTIALS"
+        value: "/var/run/secret/cloud.google.com/service-account.json"              
+    volumeMounts:      
+      - name: kube-config
+        mountPath: /root/.kube     
     resources:
       requests:
         memory: "256Mi"
         cpu: "200m"
       limits:
         memory: "256Mi"
-        cpu: "200m"
+        cpu: "200m"  
   volumes:
   - name: kube-config
     secret:
-        secretName: "${pipelineParams.environment}-kube-config"
+        secretName: "${pipelineParams.environment}-kube-config"                    
 """
     ) {
         node(POD_LABEL) {
             git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
-            
-            // Adding the "Export Kubeconfig Secret" stage
-          /*  stage('Export Kubeconfig Secret') {
-                container(name: 'egov-deployer', shell: '/bin/sh') {
-                       sh """
-                        # Create the .kube directory
-                        mkdir -p /root/.kube
-                        
-                        # Extract the kubeconfig from the secret and write it to a file
-                        kubectl get secret ${pipelineParams.environment}-kube-config -n jenkins -o jsonpath='{.data.config}' | base64 -d > /root/.kube/config
-                        
-                        # Optionally, set KUBECONFIG environment variable to use this kubeconfig
-                        export KUBECONFIG=/root/.kube/config
-                        kubectl config get-contexts
-                        kubectl config current-context
-                        aws-iam-authenticator version
-                        kubectl get nodes
-                    """
+                stage('Deploy Images') {
+                        container(name: 'egov-deployer', shell: '/bin/sh') {
+                            sh """
+                                /opt/egov/egov-deployer deploy --helm-dir `pwd`/${pipelineParams.helmDir} -c=${env.CLUSTER_CONFIGS}  -e ${pipelineParams.environment} "${env.IMAGES}"
+                            """
+                            }
                 }
-            } */
-
-            stage('Export Kubeconfig Secret') {
-                container(name: 'egov-deployer', shell: '/bin/sh') {
-                       sh """
-                        # Create the .kube directory
-                        mkdir -p /root/.kube
-                        
-                        # Extract the kubeconfig from the secret and write it to a file
-                        kubectl get secret ${pipelineParams.environment}-kube-config -n egov -o jsonpath='{.data.config}' | base64 -d > /root/.kube/config
-                        
-                        # Optionally, set KUBECONFIG environment variable to use this kubeconfig
-                        export KUBECONFIG=/root/.kube/config-bkppp
-                        kubectl config get-contexts
-                        kubectl get ns
-                        kubectl config current-context
-                        aws-iam-authenticator version
-                        kubectl get nodes
-                    """
-                }
-            }
-            
-            stage('Deploy Images') {
-                container(name: 'egov-deployer', shell: '/bin/sh') {
-                    sh """
-                        /opt/egov/egov-deployer deploy --helm-dir `pwd`/${pipelineParams.helmDir} -c=${env.CLUSTER_CONFIGS}  -e ${pipelineParams.environment} "${env.IMAGES}"
-                    """
-                }
-            }
         }
     }
+
+
 }
