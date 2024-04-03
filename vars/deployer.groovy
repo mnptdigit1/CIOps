cbd3b5abb66f21bbb3a4a9da5da08f75d4cc96b0
mnptdigit <149570527+mnptdigit@users.noreply.github.com>
2024-04-03 14:57:39 +0530

    Update deployer.groovy
    update deployer.groovey for kubecconfig error in deploy to qa

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -33,6 +33,27 @@ spec:
 """
     ) {
         node(POD_LABEL) {
+          git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
+           
+            // Adding the "Export Kubeconfig Secret" stage
+            stage('Export Kubeconfig Secret') {
+                container(name: 'egov-deployer', shell: '/bin/sh') {
+                    sh """
+                        # Create the .kube directory
+                        mkdir -p /root/.kube
+                        
+                        # Extract the kubeconfig from the secret and write it to a file
+                        kubectl get secret ${pipelineParams.environment}-kube-config -n egov -o jsonpath='{.data.config}' | base64 -d > /root/.kube/config
+                        
+                        # Optionally, set KUBECONFIG environment variable to use this kubeconfig
+                        export KUBECONFIG=/root/.kube/config/config
+                        kubectl config get-contexts
+                        kubectl config current-context
+                        aws-iam-authenticator version
+                        kubectl get nodes
+                    """
+                }
+          
             git url: pipelineParams.repo, branch: pipelineParams.branch, credentialsId: 'git_read'
                 stage('Deploy Images') {
                         container(name: 'egov-deployer', shell: '/bin/sh') {
